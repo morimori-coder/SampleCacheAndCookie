9ed5e38fa6aa364e7581de3cc31d400c113b774c
morimori-coder <67218438+morimori-coder@users.noreply.github.com>
2023-03-11 06:46:47 +0900

    不要なコメントを削除

--- SampleCacheAndCookie/Program.cs
+++ SampleCacheAndCookie/Program.cs
@@ -4,9 +4,6 @@ using SampleCacheAndCookie;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-//builder.Services.AddControllers();
 builder.Services.AddControllersWithViews();
 builder.Services.AddSingleton<ActiveSessionRepository>();
 builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
